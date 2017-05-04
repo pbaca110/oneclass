50158b4c7204ab506a5df5b2e4ae03e911b29a54
Paul Baca <pbaca110@cnm.edu>
2017-05-04 13:50:43 -0600

    finished creating my constructor

--- class/profile.php
+++ class/profile.php
@@ -1,1 +1,33 @@
 <?php
+
+namespace Edu\Cnm\DataDesign;
+class profile implements \jsonSerializable {
+	/**
+	 * @var int $profileId
+	 **/
+	private $profileId;
+	/**
+	 * @var string $profileAtHandle
+	 **/
+	private $profileAthandle;
+	/**
+	 * $var $profileActivationToken
+	 **/
+	private $profileActivationToken;
+
+	/**
+	 * constructor for profile
+	 * @param int|null $newProfileId id of this profile or null if a new profile
+	 * @param string $newProfileActivationToken
+	 * @param string $newProfileAtHandle
+	 * @throws \InvalidArgumentException if data types are not valid
+	 * @throws \RangeException if data values are out of bounds
+	 * @throws \TypeError if data types violate type hints
+	 * @throws \Exception if some other exception occurs
+	 **/
+	public function __construct(?int $newProfileId, ?string $newProfileActivationToken, ?string $newProfileAtHandle){try{$this->setProfileId($newProfileId);
+		$this->setProfileActivationToken($newProfileActivationToken);
+	$this->setProfileAtHandle($newProfileAtHandle);}
+	catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception){$exceptionType = get_class($exception);throw(new $exceptionType($exception->getMessage(),0, $exception));
+	}
+	}
