b58bf81d4515cfc541579fac998618d1cc7d59c0
Paul Baca <pbaca110@cnm.edu>
2017-05-07 13:48:22 -0600

    working on index for profile

--- public_html/api/profile/index.php
+++ public_html/api/profile/index.php
@@ -1,4 +1,68 @@
 <?php
+require_once(dirname(__DIR__, 3) . "/vendor/autoload.php");
+require_once(dirname(__DIR__, 3) . "/php/classes/autoload.php");
+require_once(dirname(__DIR__, 3) . "/php/lib/xsrf.php");
+require_once("/etc/apache2/datadesign-mysql/encrypted-config.php");
+use Edu\Cnm\DataDesign\ {
+	Profile
+};
 
+/**
+ * API for profile
+ **/
 
+if(session_status() !== PHP_SESSION_ACTIVE) {
+	session_start();
+}
+$reply = new stdClass();
+$reply->status = 200;
+$reply->data = null;
+try {
+	$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/ddctwitter.ini");
+	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
+	$id = filter_input(INPUT_GET, "id", FILTER_VALIDATE_INT);
+	$profileAtHandle = filter_input(INPUT_GET, "profileAtHandle", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+	if(($method === "DELETE" || $method === "PUT") && (empty($id) === true || $id < 0)) {
+		throw(new InvalidArgumentException("id cannot be empty or negative", 405));
+	}
+	if($method === "GET") {
+		//set XSRF cookie
+		setXsrfCookie();
+		//gets a post by content
+		if(empty($id) === false) {
+			$profile = Profile::getProfileByProfileId($pdo, $id);
+			if($profile !== null) {
+				$reply->data = $profile;
+			}
+		} else if(empty($profileAtHandle) === false) {
+			$profile = Profile::getProfileByProfileAtHandle($pdo, $profileAtHandle);
+			if($profile !== null) {
+				$reply->data = $profile;
+			}
+		}
+	} elseif($method === "PUT") {
+		//enforce the user is signed in and only trying to edit their own profile
+		if(empty($_SESSION["profile"]) === true || $_SESSION["profile"]->getProfileId() !== $id) {
+			throw(new \InvalidArgumentException("You are not allowed to access this profile", 403));
+		}
+		//decode the response from the front end
+		$requestContent = file_get_contents("php://input");
+		$requestObject = json_decode($requestContent);
+		$profile = Profile::getProfileByProfileId($pdo, $id);
+
+		if($profile === null) {
+			throw(new RuntimeException("Profile does not exist", 404));
+		}
+		if(empty($requestObject->newPassword) === true) {
+			//enforce that the XSRF token is present in the header
+			verifyXsrf();
+			//profile at handle
+			if(empty($requestObject->profileAtHandle) === true) {
+				throw(new \InvalidArgumentException ("No profile at handle", 405));
+			}
+			$profile->setProfileAtHandle($requestObject->profileAtHandle);
+			$profile->update($pdo);
+			// update reply
+			$reply->message = "Profile information updated";
+		}
 
