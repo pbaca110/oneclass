d1790d4ade870f44d6805d603394283555b83801
Paul Baca <pbaca110@cnm.edu>
2017-05-07 22:54:17 -0600

    finished sign up

--- public_html/sign-in/index.php
+++ public_html/sign-in/index.php
@@ -1,37 +1,59 @@
 <?php
-
-require_once dirname(__DIR__,3) . "/php/classes/autoload.php";
-require_once dirname(__DIR__,3) . "/php/lib/xsrf.php";
-require_once ("/etc/apache2/datadesign-mysql/encrypted-config.php");
-use Edu\Cnm\dataDesign\profile;
-
+require_once dirname(__DIR__, 3) . "/php/classes/autoload.php";
+require_once dirname(__DIR__, 3) . "/php/lib/xsrf.php";
+require_once ("/etc/apache2/data/encrypted-config.php");
+use Edu\Cnm\DataDesign\Profile;
 /**
  * api for handling sign-in
- **/
 
+ **/
+//prepare an empty reply
 $reply = new stdClass();
 $reply->status = 200;
 $reply->data = null;
-try{
-
-	if(session_status() !==PHP_SESSION_ACTIVE){
-		session_start();}
-		$pdo = connectToEncryptedMySQL("/etc/apache2/datadesign-mysql");
-
-	$method = array_key_exists("HTTP_X_HTTP_METHOD",$_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
-
-	if($method ==="POST") {verifXsrf();
-
-	$requescontent = file_get_contents();
-	$requestobject = json_encode($requescontent);
-	if(empty($requestobject->profileEmail) ===true)
-	{throw(new \InvalidArgumentException("wrong email adress.",401));
-	}else{
-		$profileEmail = filter_var($requestobject->profileEmail,FILTER_SANITIZE_EMAIL);}
-	if(empty($requestobject->profilePassword)===true){throw(new \InvalidArgumentException("must enter a password.",401));
-	}else{
-		$profilepassword = $requestobject->profilepassword;
-
+try {
+	//start session
+	if(session_status() !== PHP_SESSION_ACTIVE) {
+		session_start();
 	}
+	$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/ddctwitter.ini");
+	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
+	if($method === "POST") {
+		verifyXsrf();
+		$requestContent = file_get_contents("php://input");
+		$requestObject = json_decode($requestContent);
+		if(empty($requestObject->profileEmail) === true) {
+			throw(new \InvalidArgumentException("Wrong email address.", 401));
+		} else {
+			$profileEmail = filter_var($requestObject->profileEmail, FILTER_SANITIZE_EMAIL);
+		}
+		if(empty($requestObject->profilePassword) === true) {
+			throw(new \InvalidArgumentException("Must enter a password.", 401));
+		} else {
+			$profilePassword = $requestObject->profilePassword;
+		}
+		$profile = Profile::getProfileByProfileEmail($pdo, $profileEmail);
+		if(empty($profile) === true) {
+			throw(new \InvalidArgumentException("Invalid Email", 401));
+		}
+		if($profile->getProfileActivationToken() !== null){
+			throw (new \InvalidArgumentException ("you are not allowed to sign in unless you have activated your account", 403));
+		}
+		$hash = hash_pbkdf2("sha512", $profilePassword, $profile->getProfileSalt(), 262144);
+		if($hash !== $profile->getProfileHash()) {
+			throw(new \InvalidArgumentException("Password or email is incorrect."));
+		}
+		$profile = Profile::getProfileByProfileId($pdo, $profile->getProfileId());
+		$_SESSION["profile"] = $profile;
+		$reply->message = "Sign in was successful.";
+	} else {
+		throw(new \InvalidArgumentException("Invalid HTTP method request."));
 	}
+} catch(Exception $exception) {
+	$reply->status = $exception->getCode();
+	$reply->message = $exception->getMessage();
+} catch(TypeError $typeError) {
+	$reply->status = $typeError->getCode();
+	$reply->message = $typeError->getMessage();
 }
+header("Content-type: application/json");
